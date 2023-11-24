7ca5e60c9485af647fc5a7608fd6099b622bf309
mithu-mawhinney <mithumk4@gmail.com>
2023-11-24 16:57:11 +0000

    mymvcapp with User model

---
+++ mymvcapp/Models/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace People.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
