c9cee01ebecdc6d6ac6b6307656757bb13365d3e
mithu-mawhinney <mithumk4@gmail.com>
2023-11-30 17:50:35 +0000

    Add new project for PartyInvites

---
+++ PartyInvites/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartyInvites.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
