709b09c2cc46232dd0bd0e760b51a5eef9a1db54
mithu-mawhinney <mithumk4@gmail.com>
2024-04-30 17:13:56 +0100

    add population file in the Platform project

---
+++ Platform/Population.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Platform
+{
+    public class Population
+    {
+        private RequestDelegate next;
+
+        public Population()
+        {
+
+        }
+        public Population(RequestDelegate nextDelegate)
+        {
+            this.next = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string[] parts = context.Request.Path.ToString().Split('/', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 2 && parts[0] == "population")
+            {
+                string city = parts[1];
+                int? pop = null;
+                switch(city.ToLower())
+                {
+                    case "london":
+                        pop = 8_10_800;
+                        break;
+                    case "paris":
+                        pop = 2_04_100;
+                        break;
+
+                }
+                if (pop.HasValue)
+                {
+                    await context.Response.WriteAsync($"City: {city}, Population: {pop}");
+                    return;
+                }
+            }
+            if(next != null)
+            {
+                await next(context);
+            }
+        }
+    }
+}
